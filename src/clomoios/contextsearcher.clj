347a9fb1077933db411528605ab19220200592f7
dakrone <lee@writequit.org>
2010-05-10 12:54:02 -0600

    Refactor common searching code into core. Initial seeded contex searcher added.
    Marked all common methods that shouldn't be used as private in clomoios.core.
    These methods should primarily be used only from the contextsearching and
    seededcontextsearcher. Unless you know what you are doing.

--- src/clomoios/contextsearcher.clj
+++ src/clomoios/contextsearcher.clj
@@ -1,113 +1,6 @@
 (ns clomoios.contextsearcher
-  (:use [clojure.contrib.math :only [abs]])
-  (:use [clojure.contrib.seq-utils :only [indexed]])
-  (:require [opennlp.nlp :as nlp])
-  (:require [opennlp.tools.filters :as nlpf]))
-
-
-(defn- mindist
-  "Give the minimum distance from the first arg to any value in the second arg."
-  [n ns]
-  (apply min (map #(abs (- n %)) ns)))
-
-
-(defn- score-word
-  "Score the index-term based on it's distance from any intex-term in the given
-  search list. Use the base score to boost matches from 1 up to another value."
-  [iword iterms base]
-  (let [dist (mindist (first iword) iterms)
-        score (if (zero? dist)
-                base
-                (/ base (* 2 dist)))]
-    (if (> dist 2) 0 score)))
-
-
-(defn- score-words
-  "Score a list of words linearly based on how far they are from the
-  term.  Base score is optional and is 1 by default.  Case sensitive."
-  ([term words] (score-words term words 1))
-  ([term words base]
-   (let [iwords (indexed words)
-         iterms (map first (filter (fn [e] (= (second e) term)) iwords))]
-     (if (= 0 (count iterms))
-       (map #(vector % 0) words)
-       (map #(vector (second %) (score-word % iterms base)) iwords)))))
-
-
-(defn- nv-filter
-  "Filter tagged sentences by noun, verb and >= 3 characters."
-  [tagged-sentence]
-  (filter #(>= (count (first %)) 3) (nlpf/nouns-and-verbs tagged-sentence)))
-
-
-(defn- contains-token?
-  "Given a sentence, does the given term exist in that sentence?"
-  [sentence term tokenizer]
-  (let [tokens (tokenizer sentence)]
-    (boolean (some #{term} tokens))))
-
-
-(defn- get-matching-sentences
-  "Given a sequence of sentences, return the sentences containing
-  the term."
-  [sentences term tokenizer]
-  (filter #(contains-token? % term tokenizer) sentences))
-
-
-(defn- get-tagged-sentences
-  "Return a sequence of POS-tagged sentences."
-  [sentences pos-tagger tokenizer]
-  (map #(pos-tagger (tokenizer %)) sentences))
-
-
-(defn- get-weighted-sentences
-  "Given POS-tagged sentences and a term, return a sequence of
-  sentences that have been weighted."
-  [tagged-sentences term]
-  (map #(score-words term (map first (nv-filter %))) tagged-sentences))
-
-
-(defn- get-new-terms
-  "Given a sequence of weighted sentences, return a map of new terms
-  to be used for searching."
-  [weighted-sentences]
-  (into {} (map vec (partition 2 (flatten (map #(filter (fn [pair] (not= 0 (second pair))) %) weighted-sentences))))))
-
-
-(defn- get-scored-terms
-  "Given a block of text and a search term, return a map of new search
-  terms as keys with weighted score values."
-  [text term sentence-finder tokenizer pos-tagger]
-  (let [sentences (sentence-finder text)
-        matched-sentences (get-matching-sentences sentences term tokenizer)
-        tagged-sentences (get-tagged-sentences matched-sentences pos-tagger tokenizer)
-        weighted-sentences (get-weighted-sentences tagged-sentences term)
-        new-terms (get-new-terms weighted-sentences)]
-    new-terms))
-
-
-(defn- score-sentence
-  "Given a sentence and a map of words & their scores, return the score
-  of the sentence."
-  [sentence score-words tokenizer]
-  (let [tokens (tokenizer sentence)]
-    (reduce + (map #(get score-words % 0) tokens))))
-
-
-(defn- score-sentences
-  "Given a text and a map of words/scores. Return a list of sentences
-  and their scores."
-  [text score-words sentence-finder tokenizer]
-  (let [sentences (sentence-finder text)]
-    (for [s sentences]
-      [s (score-sentence s score-words tokenizer)])))
-
-
-(defn- score-text
-  "Score a block of text, given a map of score-words."
-  [text score-words sentence-finder tokenizer]
-  (let [sentences (sentence-finder text)]
-    (reduce + (map #(score-sentence % score-words tokenizer) sentences))))
+  (:use [clomoios.core :as core])
+  (:require [opennlp.nlp :as nlp]))
 
 
 (defprotocol Searcher
@@ -125,16 +18,16 @@
     (let [get-sentences (:get-sentences this)
           tokenizer (:tokenize this)
           pos-tagger (:pos-tag this)
-          score-words (get-scored-terms text term get-sentences tokenizer pos-tagger)]
-      (score-text text score-words get-sentences tokenizer)))
+          score-words (core/get-scored-terms text term get-sentences tokenizer pos-tagger)]
+      (core/score-text text score-words get-sentences tokenizer)))
 
   (rank
     [this term text]
     (let [get-sentences (:get-sentences this)
           tokenizer (:tokenize this)
           pos-tagger (:pos-tag this)
-          score-words (get-scored-terms text term get-sentences tokenizer pos-tagger)]
-      (reverse (sort-by second (score-sentences text score-words get-sentences tokenizer))))))
+          score-words (core/get-scored-terms text term get-sentences tokenizer pos-tagger)]
+      (reverse (sort-by second (core/score-sentences text score-words get-sentences tokenizer))))))
 
 
 (defn make-context-searcher
@@ -148,13 +41,15 @@
     (ContextSearcher. get-sentences tokenizer pos-tagger)))
 
 
-;(def cs (make-context-searcher "models/EnglishSD.bin.gz" "models/EnglishTok.bin.gz" "models/tag.bin.gz"))
-;(println (score cs "foo" "This is a sentence foo is in. In this one foo goes fishing with Tom."))
-;(println (rank cs "foo" "This is a sentence foo is in. In this one foo goes fishing with Tom."))
-;(println "---------------------")
-;(println (score cs "foo" "This is a sentence foo is in."))
-;(println (rank cs "foo" "This is a sentence foo is in."))
-;(println "---------------------")
-;(println (score cs "foo" "This is a sentence bar is in."))
-;(println (rank cs "foo" "This is a sentence bar is in."))
+(comment
 
+  (use 'clomoios.contextsearcher)
+  (def cs (make-context-searcher "models/EnglishSD.bin.gz" "models/EnglishTok.bin.gz" "models/tag.bin.gz"))
+  (score cs "foo" "This is a sentence foo is in. In this one foo goes fishing with Tom.")
+  (rank cs "foo" "This is a sentence foo is in. In this one foo goes fishing with Tom.")
+  (score cs "foo" "This is a sentence foo is in.")
+  (rank cs "foo" "This is a sentence foo is in.")
+  (score cs "foo" "This is a sentence bar is in.")
+  (rank cs "foo" "This is a sentence bar is in.")
+
+)
---
+++ src/clomoios/seededcontextsearcher.clj
@@ -0,0 +1,65 @@
+(ns clomoios.seededcontextsearcher
+  (:use [clomoios.core :as core])
+  (:require [opennlp.nlp :as nlp]))
+
+
+(defprotocol SeededSearcher
+  "An interface for searching using seeded text"
+  (add-seed [this seed]      "Add seed text to this searcher")
+  (score    [this term text] "Score this text in similarity")
+  (rank     [this term text] "Rank sentences in this text"))
+
+
+(defrecord SeededContextSearcher [seedtexts get-sentences tokenize pos-tag])
+
+(extend-protocol SeededSearcher SeededContextSearcher
+
+  (add-seed
+    [this seedtext]
+    (let [get-sentences (:get-sentences this)
+          tokenizer (:tokenize this)
+          pos-tagger (:pos-tag this)
+          seedtexts (:seedtexts this)]
+      (swap! seedtexts concat [seedtext])))
+
+  (score
+    [this term text]
+    (let [get-sentences (:get-sentences this)
+          tokenizer (:tokenize this)
+          pos-tagger (:pos-tag this)
+          seedtexts (:seedtexts this)
+          score-words (into {}
+                            (map #(core/get-scored-terms % term get-sentences tokenizer pos-tagger) @seedtexts))]
+      (core/score-text text score-words get-sentences tokenizer)))
+
+  (rank
+    [this term text]
+    (let [get-sentences (:get-sentences this)
+          tokenizer (:tokenize this)
+          pos-tagger (:pos-tag this)
+          seedtexts (:seedtexts this)
+          score-words (into {}
+                            (map #(core/get-scored-terms % term get-sentences tokenizer pos-tagger) @seedtexts))]
+      (reverse (sort-by second (core/score-sentences text score-words get-sentences tokenizer))))))
+
+
+(defn make-seeded-context-searcher
+  "Generate a new Seeded Context Searcher using the seed text and given models.
+  3 models are required, a sentence detector model, a tokenizing model and a
+  pos-tagging model."
+  [seedtext smodel tmodel pmodel]
+  (let [get-sentences (nlp/make-sentence-detector smodel)
+        tokenizer (nlp/make-tokenizer tmodel)
+        pos-tagger (nlp/make-pos-tagger pmodel)]
+    (SeededContextSearcher. (atom [seedtext]) get-sentences tokenizer pos-tagger)))
+
+
+(comment
+
+  (use 'clomoios.seededcontextsearcher)
+  (def foo (make-seeded-context-searcher "The quick brown fox jumped over the sleeping lazy dog biscuit."  "models/EnglishSD.bin.gz" "models/EnglishTok.bin.gz" "models/tag.bin.gz"))
+  (rank foo "fox" "I own a brown fox. He's my favorite pet. I like him more than my dog.")
+  (add-seed foo "I have a pet fox.")
+  (rank foo "fox" "I own a brown fox. He's my favorite pet. I like him more than my dog.")
+
+)

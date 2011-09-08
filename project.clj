232383f807bb0c95dd753c8ae02b1a2e975e2461
Lee Hinman <lee@writequit.org>
2011-09-07 22:52:25 -0600

    bump to 1.3 beta

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clomoios "0.1.1-SNAPSHOT"
   :description "Context searching using NLP magic"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clojure-opennlp "0.1.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
